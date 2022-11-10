600d7d5fd7a7b8ec9f8297c0234f9548f4a564e9
maikully <zeebli123@gmail.com>
2022-11-09 19:45:43 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import "./App.css";
-import { useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+import './App.css'
+import { useState } from 'react'
+import bakeryData from './assets/bakery-data.json'
+import BakeryItem from './components/BakeryItem'
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
-bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
-});
+bakeryData.forEach(item => {
+  item.image = process.env.PUBLIC_URL + '/' + item.image
+})
 /* ############################################################## */
 
-function App() {
+function App () {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [cart, changeCart] = useState([])
+  const [total, changeTotal] = useState(0)
+  const handleClick = (item) => {
+    changeCart([...cart, item.name])
+    changeTotal(total + item.price)
+  }
   return (
-    <div className="App">
+    <div className='App'>
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((
+        item,
+        index // TODO: map bakeryData to BakeryItem components
+      ) => (
+        
+        <div><button onClick={() =>handleClick(item)}><BakeryItem info={item}/></button></div>
+        // replace with BakeryItem component
       ))}
-
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <ol> 
+          {cart.map((
+            item,
+            index
+          ) => (
+            <li> {item}</li>
+          ))}
+        </ol>
+        {total}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
 // TODO: create a component that displays a single bakery item
+import { Component } from 'react'
+
+class BakeryItem extends Component {
+  constructor (props) {
+    super(props)
+    console.log(props)
+  }
+  render () {
+    return (
+      <div>
+        <img src={this.props.info.image} style={{width:"20%"}}></img>
+        <h3>{this.props.info.name}</h3>
+        <p>{this.props.info.description}</p>
+      </div>
+    )
+  }
+}
+export default BakeryItem
